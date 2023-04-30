d455247d04d2b42252c8f0493e2f79996e7a7e0a
Jæcob <82502626+Jacobzy2137@users.noreply.github.com>
2023-04-30 12:54:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import cv2
 import os
+import requests
 import urllib.request
 import numpy as np
 from PIL import Image
-from io import BytesIO
 from fastapi import FastAPI
 
 
-def find_white_background(imgpath, threshold=0.45):
+app = FastAPI()
+@app.get('/{url}')
+async def root():
+    img_path = 'image.jpg'
+    urllib.request.urlretrieve(url, img_path)
+    threshold = 0.35
     img_array = cv2.imread(imgpath)
     background = np.array([255, 255, 255])
     percent = (img_array == background).sum() / img_array.size
     if percent >= threshold:
-        print(percent)
-        return True
+        x = {'packshot': 'obraz to packshot'}
     else:
-        return False
+        x = {'packshot': 'obraz nie jest packshotem'}
 
-img_path = 'image.jpg'
-packshot_check = find_white_background(img_path)
+    if os.path.exists(img_path):
+      os.remove(img_path)
 
-
-
-if packshot_check == True:
-    print('Ten obrazek to PACKSHOT')
-else:
-    print('ten obrazek NIE JEST PACKSHOTEM')
-
-if os.path.exists(img_path):
-    os.remove(img_path)
+    return x
